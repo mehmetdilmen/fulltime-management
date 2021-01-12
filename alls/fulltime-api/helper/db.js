17b59617fff353254b3621364fad4cc93bae24d1
Mehmet Dilmen <43604706+mehmetdilmen@users.noreply.github.com>
2021-01-12 19:49:59 +0300

    Update db.js

--- alls/fulltime-api/helper/db.js
+++ alls/fulltime-api/helper/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-    mongoose.connect('mongodb://mehmetdilmen:Sevenfold12@cluster0-shard-00-00-7isja.gcp.mongodb.net:27017,cluster0-shard-00-01-7isja.gcp.mongodb.net:27017,cluster0-shard-00-02-7isja.gcp.mongodb.net:27017/test?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin', {useMongoClient: true});
+    mongoose.connect('mongodb://mehmetdilmen:KPROjrnGHRtsyDDX@cluster0-shard-00-00-7isja.gcp.mongodb.net:27017,cluster0-shard-00-01-7isja.gcp.mongodb.net:27017,cluster0-shard-00-02-7isja.gcp.mongodb.net:27017/test?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin', {useMongoClient: true});
     mongoose.connection.on('open', () => {
         console.log('DB Bağlandı');
     });
